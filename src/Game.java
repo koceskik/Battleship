c5a0bfa878543a671796603b63f17547296fd18b
Kyle-ARCH <koceskik@gmail.com>
2013-10-25 18:12:53 -0400

    Initial commit

---
+++ src/Game.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+
+public class Game {
+	ArrayList<Player> players = new ArrayList<Player>();
+	
+	public Game() {
+		
+	}
+	
+	public void applyMove(Move m) {
+		
+	}
+
+}
